a495293be12ca5dd9ddf19e99329fb1eb201bd28
martken <martin.kenirons@atu.ie>
2025-10-20 09:30:58 +0000

    lab4

--- src/components/create.jsx
+++ src/components/create.jsx
@@ -1,9 +1,50 @@
-const Create = ()=>{
-    return(
-        <div>
-            <h1>Helllo from create component!</h1>
+// create.js
+
+import { useState } from "react";
+
+function Create() {
+  const [title, setTitle] = useState('');
+  const[ year, setYear] = useState('');
+  const[poster, setPoster] = useState('');
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title,year,poster);
+
+  }
+
+  return (
+    <div>
+      <h2>This is my Create Component.</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Title: </label>
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
         </div>
-    );
+        <div className="form-group">
+          <label>Add Movie Year: </label>
+          <input type="text"
+            className="form-control"
+            value={year}
+            onChange={(e) => { setYear(e.target.value) }}
+          />
+        </div>
+         <div className="form-group">
+          <label>Add Movie Year: </label>
+          <textarea type="text"
+            className="form-control" 
+            value={poster}
+            onChange={(e) => { setPoster(e.target.value) }}
+          />
+        </div>
+        <input type="submit" value="Add Movie" />
+      </form>
+    </div>
+  );
 }
 
 export default Create;
